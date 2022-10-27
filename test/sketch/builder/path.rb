b1448e055566775b95e8cbe4c812fac28100473b
Connor Moore <cmoore@aurorasolar.com>
2022-10-27 14:57:58 -0700

    fixed path.rb deprecation warnings

--- test/sketch/builder/path.rb
+++ test/sketch/builder/path.rb
@@ -2,12 +2,12 @@ require 'minitest/autorun'
 require 'sketch/builder/path'
 
 describe Sketch::Builder::Path do
-    Path = Sketch::Path
-    Point = Geometry::Point
+  Path = Sketch::Path
+  Point = Geometry::Point
 
-    let(:builder) { Sketch::Builder::Path.new }
+  let(:builder) { Sketch::Builder::Path.new }
 
-    it "must build a Path from a block" do
-	builder.evaluate {}.must_be_kind_of Path
-    end
+  it "must build a Path from a block" do
+    _(builder.evaluate {}).must_be_kind_of Path
+  end
 end
